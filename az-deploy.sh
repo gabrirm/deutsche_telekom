5ac35fe0bfb71ea50cb2689506b70f03756b1162
Gabriel <gabrirm00@gmail.com>
2024-05-29 22:55:03 +0200

    fixed error

--- az-deploy.sh
+++ az-deploy.sh
@@ -5,8 +5,9 @@ LOCATION="westeurope"
 SITE_NAME="deutschetelekom"
 PLAN_NAME="ServicePlan"
 RESOURCE_GROUP="rg_deutschetelekom"
-PHP_VERSION="8.3"
+PHP_VERSION="8.0"
 GITHUB_REPO="https://github.com/gabrirm/deutsche_telekom.git"
+BRANCH="main"
 
 
 # Log in to Azure using github secrets
@@ -16,8 +17,8 @@ az login --service-principal -u $AZ_CLIENT_ID -p $AZ_CLIENT_SECRET --tenant $AZ_
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
 # Create an App Service plan
-az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --location $LOCATION --is-linux --sku FREE
+az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --location $LOCATION --is-linux --sku F1
 
 # Create webapp
-az webapp create --name $SITE_NAME --resource-group $RESOURCE_GROUP --plan $PLAN_NAME  --deployment-source-url $GITHUB_REPO -b main --runtime "PHP|$PHP_VERSION"
+az webapp create --name $SITE_NAME --resource-group $RESOURCE_GROUP --plan $PLAN_NAME  --deployment-source-url $GITHUB_REPO -b $BRANCH --runtime "PHP|$PHP_VERSION"
 az webapp config set --resource-group $RESOURCE_GROUP --name $SITE_NAME --php-version $PHP_VERSION 
