9fd02896ce62b02b73c9b2d5f392dcee9b6a6d58
Gabriel <gabrirm00@gmail.com>
2024-05-29 21:35:14 +0200

    fixed error

--- az-deploy.sh
+++ az-deploy.sh
@@ -19,5 +19,5 @@ az group create --name $RESOURCE_GROUP --location $LOCATION
 az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --location $LOCATION --sku FREE
 
 # Create webapp
-az webapp create --name $SITE_NAME --resource-group $RESOURCE_GROUP --plan $PLAN_NAME --location $LOCATION --deployment-source-url $GITHUB_REPO --branch "main" --runtime "PHP|$PHP_VERSION"
+az webapp create --name $SITE_NAME --resource-group $RESOURCE_GROUP --plan $PLAN_NAME  --deployment-source-url $GITHUB_REPO --branch "main" --runtime "PHP|$PHP_VERSION"
 az webapp config set --resource-group $RESOURCE_GROUP --name $SITE_NAME --php-version $PHP_VERSION
