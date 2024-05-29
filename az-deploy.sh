c977b6c54f6cf16f9ddc8e9d0f461f5b2be3c981
Gabriel <gabrirm00@gmail.com>
2024-05-29 21:32:28 +0200

    added creation of service plan

--- az-deploy.sh
+++ az-deploy.sh
@@ -3,6 +3,7 @@
 # GLOBAL VARIABLES
 LOCATION="westeurope"
 SITE_NAME="deutschetelekom"
+PLAN_NAME="deutschetelekom-plan"
 RESOURCE_GROUP="rg_deutschetelekom"
 PHP_VERSION="8.3"
 GITHUB_REPO="https://github.com/gabrirm/deutsche_telekom.git"
@@ -14,6 +15,9 @@ az login --service-principal -u $AZ_CLIENT_ID -p $AZ_CLIENT_SECRET --tenant $AZ_
 # Create a resource group
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
+# Create an App Service plan
+az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --location $LOCATION --sku FREE
+
 # Create webapp
-az webapp create --name $SITE_NAME --resource-group $RESOURCE_GROUP --location $LOCATION --deployment-source-url $GITHUB_REPO --branch "main" --runtime "PHP|$PHP_VERSION"
+az webapp create --name $SITE_NAME --resource-group $RESOURCE_GROUP --plan $PLAN_NAME --location $LOCATION --deployment-source-url $GITHUB_REPO --branch "main" --runtime "PHP|$PHP_VERSION"
 az webapp config set --resource-group $RESOURCE_GROUP --name $SITE_NAME --php-version $PHP_VERSION
