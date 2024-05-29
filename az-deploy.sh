7c652f2d78905fecdd7ddb10a7848aaeb249aaf0
Gabriel <gabrirm00@gmail.com>
2024-05-29 21:44:04 +0200

    fixed error

--- az-deploy.sh
+++ az-deploy.sh
@@ -3,7 +3,7 @@
 # GLOBAL VARIABLES
 LOCATION="westeurope"
 SITE_NAME="deutschetelekom"
-PLAN_NAME="deutschetelekom-plan"
+PLAN_NAME="ServicePlan"
 RESOURCE_GROUP="rg_deutschetelekom"
 PHP_VERSION="8.3"
 GITHUB_REPO="https://github.com/gabrirm/deutsche_telekom.git#main"
